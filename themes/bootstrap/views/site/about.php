e8b51b60afe67489ade69b3e4408d2dd9da4b2df
omda <eabdulwahid@wallpostdev.com>
2021-07-12 11:50:16 +0200

    the about file has been edited

--- themes/bootstrap/views/site/about.php
+++ themes/bootstrap/views/site/about.php
@@ -7,8 +7,9 @@
                 <h1>About</h1>
                 <hr>
                 <p class="lead">
-                    This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.
-                    This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.
+                    this is test blog to practice the yii framework
+                    <br>
+                    this is about us version 2
                 </p>
 
             </div>
