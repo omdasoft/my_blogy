bae68313a091587680ff633a4e5f53c07874cee7
omda <eabdulwahid@wallpostdev.com>
2021-07-12 11:50:16 +0200

    the branch ABDC first commit

--- themes/bootstrap/views/site/about.php
+++ themes/bootstrap/views/site/about.php
@@ -7,8 +7,7 @@
                 <h1>About</h1>
                 <hr>
                 <p class="lead">
-                    This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.
-                    This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.
+                    this is test blog to practice the yii framework
                 </p>
 
             </div>
